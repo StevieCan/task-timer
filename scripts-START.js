c9b19e13a21eb031380e230729873a2eeafabdd3
Steve Han <shan61489@gmail.com>
2018-12-10 12:25:10 -0600

    complete functionality

--- scripts-START.js
+++ scripts-START.js
@@ -1,13 +1,21 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
-  setInterval(() => {
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check if we should stop it!
-    if (secondsLeft < 0) {
+    if (secondsLeft <= 0) {
       clearInterval(countdown);
       return;
     }
@@ -19,6 +27,32 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-
-  console.log({minutes, remainderSeconds});
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hours = end.getHours();
+  const adjustedHour = (hours > 12 ? hours - 12 : hours);
+  const minutes = end.getMinutes();
+  const adjustedMinutes = (minutes < 10 ? '0' : '');
+  endTime.textContent = `Be Back At ${adjustedHour}:${adjustedMinutes}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+  
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  //stop it from running
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
